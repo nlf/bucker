18b0f350ab6caa96d989784278726f82641cc498
Nathan LaFreniere <quitlahok@gmail.com>
2014-06-12 11:12:28 -0700

    update test to show options

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var bucker = require('./index');
-var logger = bucker.createLogger({ console: true, influxdb: {} }, module);
+var logger = bucker.createLogger({ console: true, influxdb: { host: '127.0.0.1', port: 5551 } }, module);
 
 logger.log('hello', { test: 'data' }, 14);
 logger.module('bacon').log('hi again');
 logger.error('ruh roh').email();
-logger.stat('something', 42);
+// logger.stat('something', 42);

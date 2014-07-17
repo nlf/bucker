beecc1336c9a4185c077c5deffdd9931263a27db
Nathan LaFreniere <quitlahok@gmail.com>
2014-07-17 13:11:01 -0700

    show file transport usage

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var bucker = require('./index');
-var logger = bucker.createLogger({ console: true, influxdb: { host: '127.0.0.1', port: 5551 } }, module);
+var logger = bucker.createLogger({ console: true, influxdb: { host: '127.0.0.1', port: 5551 }, file: { filename: 'test.log' } }, module);
 
 logger.log('hello', { test: 'data' }, 14);
 logger.log('hello %s', 'bacon');

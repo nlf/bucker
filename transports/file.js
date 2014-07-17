fb2227cbeaabfea6e71b2ca9715c713248becffc
Nathan LaFreniere <quitlahok@gmail.com>
2014-07-17 09:15:59 -0700

    add a file transport

---
+++ transports/file.js
@@ -0,0 +1,84 @@
+var Util = require('util');
+var Fs = require('fs');
+var BaseTransport = require('../lib/baseTransport');
+
+var File = BaseTransport.extend(function () {
+    if (!this.options.hasOwnProperty('timestamp') || this.options.timestamp === true) {
+        this.options.timestamp = 'YYYY-MM-DDTHH:mm:ss';
+    }
+
+    if (!this.options.format) {
+        this.options.format = ':time :level:tags: :data';
+    }
+
+    if (!this.options.accessFormat) {
+        this.options.accessFormat = ':remote_ip - - [:time] ":method :url HTTP/:http_ver" :status :length :response_time ":referer" ":agent"';
+    }
+
+    this.filestream = Fs.createWriteStream(this.options.file, { encoding: 'utf8', flags: 'a+' });
+});
+
+File.prototype._write = function (line) {
+
+    this.filestream.write(line + '\n');
+};
+
+File.prototype._format = function (level, name, timestamp, tags, data) {
+
+    var line = this.options.format;
+    line = line.replace(':time', this.options.timestamp ? timestamp.format(this.options.timestamp) : '');
+    line = line.replace(':level', name ? name + '.' + level : level);
+    line = line.replace(':tags', tags.length ? '[' + tags.join(',') + ']' : '');
+    line = line.replace(':data', Util.format.apply(null, data));
+
+    return line;
+};
+
+File.prototype.debug = function (name, timestamp, tags, data) {
+
+    this._write(this._format('debug', name, timestamp, tags, data));
+};
+
+File.prototype.info = function (name, timestamp, tags, data) {
+
+    this._write(this._format('info', name, timestamp, tags, data));
+};
+
+File.prototype.warn = function (name, timestamp, tags, data) {
+
+    this._write(this._format('warn', name, timestamp, tags, data));
+};
+
+File.prototype.error = function (name, timestamp, tags, data) {
+
+    this._write(this._format('error', name, timestamp, tags, data));
+};
+
+File.prototype.exception = function (name, timestamp, tags, data) {
+
+    this._write(this._format('exception', name, timestamp, tags, data));
+};
+
+File.prototype.access = function (name, timestamp, tags, data) {
+
+    var line = this.options.accessFormat;
+    line = line.replace(':time', this.options.timestamp ? timestamp.format(this.options.timestamp) : '');
+    line = line.replace(':level', name ? name + '.access' : 'access');
+    line = line.replace(':tags', tags.length ? '[' + tags.join(',') + ']' : '');
+    line = line.replace(':method', data[0].method);
+    line = line.replace(':remote_ip', data[0].remote_ip);
+    line = line.replace(':url', data[0].url);
+    line = line.replace(':http_ver', data[0].http_ver);
+    line = line.replace(':status', data[0].status);
+    line = line.replace(':response_time', data[0].response_time);
+    line = line.replace(':length', data[0].length);
+    line = line.replace(':referer', data[0].referer);
+    line = line.replace(':agent', data[0].agent);
+
+    return this._write(line);
+};
+
+// File.prototype.stat = function (name, timestamp, tags, data) {
+// };
+
+module.exports = File;

3e983203d2406f97c32272571c89976c781bb613
Chris Moyer <cmoyer@newstex.com>
2014-08-04 13:09:24 -0400

    Fixed up sending complex logs to Loggly.

--- transports/loggly.js
+++ transports/loggly.js
@@ -11,6 +11,7 @@
  */
 'use strict';
 
+var util = require('util');
 var BaseTransport = require('../lib/baseTransport');
 var Loggly = BaseTransport.extend(function () {
     require('loggly');
@@ -37,16 +38,23 @@ Loggly.prototype._send = function (level, name, timestamp, tags, data) {
         '@name': name,
         '@tags': tags,
     };
-
-    if (typeof data === 'object'){
-        Object.keys(data).forEach(function(k){
-            payload[k] = data[k];
-        });
-    } else {
-        payload.message = data;
+    if (!Array.isArray(data)){
+        data = [data];
     }
 
-    self.client.log(JSON.stringify(payload));
+    // Process each message
+    data.forEach(function(msg){
+
+        if (typeof msg === 'object'){
+            Object.keys(msg).forEach(function(k){
+                payload[k] = msg[k];
+            });
+        } else {
+            payload.message = util.format(msg);
+        }
+
+        self.client.log(JSON.stringify(payload));
+    });
 };
 
 Loggly.prototype.stat = function (name, timestamp, tags, data) {
@@ -70,7 +78,7 @@ Loggly.prototype.error = function (name, timestamp, tags, data) {
 };
 
 Loggly.prototype.exception = function (name, timestamp, tags, data) {
-    this._send('exception', name, timestamp, tags, data);
+    this._send('exception', name, timestamp, tags, util.format(data));
 };
 
 Loggly.prototype.access = function (name, timestamp, tags, data) {
